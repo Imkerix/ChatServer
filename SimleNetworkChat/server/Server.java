3af26bb694f7b1c50c947a43683db1c815187f3e
Erik Heinisch <Imkerixus@gmail.com>
2013-12-21 23:22:55 +0100

    I found out, that protokol only needs the thread running the communication with the client and can now get all needed Stuff out of there... maybe I make protokol an extra class.

--- SimleNetworkChat/server/Server.java
+++ SimleNetworkChat/server/Server.java
@@ -2,33 +2,31 @@ package server;
 
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import utility.Msg;
 
 public class Server
 {
 	private ServerSocket serversocket = null; /** Endpoint on the Server */
-	private ArrayList<SocketThread> connectedClients = new ArrayList<SocketThread>();
-	
-
+	private List<ServerClient> connectedClients = Collections.synchronizedList(new ArrayList<ServerClient>());
 	public Server(int port) throws IOException
 	{
 		serversocket = new ServerSocket(port);
 		while (true)
 		{
-			SocketThread thread = new SocketThread(serversocket.accept())
+			ServerClient thread = new ServerClient(serversocket.accept())
 				{
 					@Override
 					public void run()
 					{
 						try
 						{
-							protokol(clientsocket,out,in,this); // this is a SocketThread
+							protokol(this); // this is a SocketThread
 						} catch (IOException e)
 						{
 							e.printStackTrace();
@@ -40,37 +38,61 @@ public class Server
 		}
 	}
 
-	private void protokol(Socket clientsocket, ObjectOutputStream out,ObjectInputStream in, SocketThread activethread) throws IOException
+	private void protokol(ServerClient activeclient) throws IOException
 	{
 		boolean shutdown = false;
 		/** Determinates if the Client wants to disconnect */
-
 		while (shutdown == false)
 		{
-			Msg nextCMD = activethread.st.readMsg(clientsocket,in);
-			while(nextCMD == null){}
-			
-			if (nextCMD.getId() == 'r')
+			Msg nextCMD = activeclient.st.readMsg(activeclient.clientsocket, activeclient.in);
+			if(nextCMD.getId() == 'b')		//// Broadcast message to everybody on the server
 			{
-				broadcast(clientsocket, out, activethread, nextCMD);
+				broadcast(activeclient.clientsocket, activeclient, nextCMD);
 			}
-			if(nextCMD.getId() == ';')
+			if(nextCMD.getId() == 's')		//// Message to a specific person on the server
 			{
-				clientsocket.close();
+			}
+			if(nextCMD.getId() == 'r')		//// Message to all people in a room
+			{
+			}
+			if(nextCMD.getId() == '?')		//// Who is online
+			{
+				whoisonline(activeclient.clientsocket, activeclient);
+			}
+			if(nextCMD.getId() == ';')		//// Closes a Connection
+			{
+				shutdownThread(activeclient.clientsocket, activeclient);
 				shutdown = true;
-				connectedClients.remove(activethread);
-				System.out.println("is down");
 			}
 		}
 	}
-	public synchronized void broadcast(Socket clientsocket, ObjectOutputStream out, SocketThread activethread, Msg p_msg)
+	
+	public void broadcast(Socket clientsocket, ServerClient activeclient, Msg p_msg)
 	{
-		for (SocketThread soth : connectedClients) // removes Thread from list of all connected clients
+		for (ServerClient soth : connectedClients) // removes Thread from list of all connected clients
 		{
-			soth.st.writeMsg(soth.clientsocket, soth.out, p_msg);
+			soth.st.writeMsg(soth.clientsocket, activeclient.out, p_msg);
 		}
-		
 		System.err.println(p_msg.getContent()+" I am the Server");
 	}
 	
+	public void shutdownThread(Socket clientsocket, ServerClient activeclient) throws IOException
+	{
+		clientsocket.close();
+		connectedClients.remove(activeclient);
+		System.out.println("is down");
+	}
+	
+	public void whoisonline(Socket clientsocket, ServerClient activeclient) throws IOException
+	{
+		ArrayList<String> clientsOnline = new ArrayList<String>(); 
+		for (ServerClient sc : connectedClients)
+		{
+			if(sc != activeclient)  // dont ask active client for its name he knows that already ^^
+			{
+					clientsOnline.add(sc.nickname);
+			}
+		}
+		activeclient.st.writeMsg(activeclient.clientsocket, activeclient.out, new Msg("",'o',clientsOnline));
+	}
 }
