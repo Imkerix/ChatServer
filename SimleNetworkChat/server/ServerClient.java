00fa06d52a9e36e2e8d0b72b0c7b1c33d1f2af4a
Erik Heinisch <Imkerixus@gmail.com>
2013-12-21 23:20:08 +0100

    I call this ServerClient now, because its the place where I save stuff about the client now, might be not a bright choise, but its easy to maintain, in and out are in here now, and I even save the socket to comunicate with the ReadClient here. StreamTools are used right from here, so I could reduce stuff in the Server class.

--- SimleNetworkChat/server/ServerClient.java
+++ SimleNetworkChat/server/ServerClient.java
@@ -7,22 +7,27 @@ import java.net.Socket;
 
 import utility.StreamTools;
 
-public class SocketThread extends Thread 
+public class ServerClient extends Thread 
 {
+	public String nickname;
 	public Socket clientsocket;
 	public ObjectOutputStream out;
 	public ObjectInputStream in;
+	
 	StreamTools st = new StreamTools();
 	
-	public SocketThread(Socket p_socket)
+	public ServerClient(Socket p_socket)
 	{
 		this.clientsocket = p_socket;
 		
-		try {
+		try 
+		{
 			out = new ObjectOutputStream(clientsocket.getOutputStream());
 			in = new ObjectInputStream(clientsocket.getInputStream());
-		} catch (IOException e) {
+		} catch (IOException e) 
+		{
 			e.printStackTrace();
 		}
+		this.nickname = st.readMsg(clientsocket, in).getContent();
 	}
 }
