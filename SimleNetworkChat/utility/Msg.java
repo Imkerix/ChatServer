7923972839f92c2256e89f895421ef8249360719
Erik Heinisch <Imkerixus@gmail.com>
2013-12-21 23:13:14 +0100

    +serialVersionUID to help checking internal java stuff while transferring Msg objects over network.
    +Object object and its getter I hold this slot free for some stuff i may
    need to transport over the stream ^^ "maybe I make object default and
    cast the rest"

--- SimleNetworkChat/utility/Msg.java
+++ SimleNetworkChat/utility/Msg.java
@@ -6,14 +6,17 @@ import java.util.Date;
 
 public class Msg implements Serializable
 {
+	private static final long serialVersionUID = 1L;
 	private String content;
+	private Object object;
 	private char id;
 	private Date date = new Date();
 
-	public Msg(String p_content, char p_id)
+	public Msg(String p_content, char p_id, Object p_object)
 	{
 		this.content = p_content;
 		this.id = p_id;
+		this.object = p_object;
 	}
 
 	public String getContent()
@@ -30,4 +33,10 @@ public class Msg implements Serializable
 	{
 		return date;
 	}
+	
+	public Object getObject()
+	{
+		return object;
+	}
+	
 }
