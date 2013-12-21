6dc92e44ab1f973a7369c0567dd728585af7a533
Erik Heinisch <Imkerixus@gmail.com>
2013-12-21 15:15:10 +0100

    just derped around ^^

--- SimleNetworkChat/Main.java
+++ SimleNetworkChat/Main.java
@@ -42,60 +42,62 @@ public class Main
 		// Comment this if graphical interface is used
 		final String ip = "127.0.0.1";
 		// Comment this if graphical interface is used
-		// JFrame frame = new JFrame("jframe");
-		// Object[] options = {"127.0.0.1","10.34.12.85"};
-		// final String ip = (String) JOptionPane.showInputDialog(
-		// frame,
-		// "Ip of the Server?",
-		// "Where is The Server ???",
-		// JOptionPane.PLAIN_MESSAGE,
-		// null,
-		// options,
-		// "10.34.12.85");
+//		 JFrame frame = new JFrame("jframe");
+//		 Object[] options = {"127.0.0.1","192.168.2.101"};
+//		 final String ip = (String) JOptionPane.showInputDialog(
+//		 frame,
+//		 "Ip of the Server?",
+//		 "Where is The Server ???",
+//		 JOptionPane.PLAIN_MESSAGE,
+//		 null,
+//		 options,
+//		 "10.34.12.85");
 		// // Small GUI to ask for IP //// Small GUI to ask for IP ////
 
-		Thread clientThread0 = new Thread() // A Client Thread
-		{
-			public void run()
-			{
-				ChatClient c = new ChatClient();
-			}
-		};
-		clientThread0.start();
-		Thread clientThread1 = new Thread() // A Client Thread
-		{
-			public void run()
-			{
-				ChatClient c = new ChatClient();
-			}
-		};
-		clientThread1.start();
-		Thread clientThread2 = new Thread() // A Client Thread
-		{
-			public void run()
-			{
-				ChatClient c = new ChatClient();
-			}
-		};
-		clientThread2.start();
-
-		Thread clientThread3 = new Thread() // A Client Thread
-		{
-			public void run()
-			{
-				ChatClient c = new ChatClient();
-			}
-		};
-		clientThread3.start();
-
-		Thread clientThread4 = new Thread() // A Client Thread
-		{
-			public void run()
-			{
-				ChatClient c = new ChatClient();
-			}
-		};
-		clientThread4.start();
+//		Thread clientThread0 = new Thread() // A Client Thread
+//		{
+//			public void run()
+//			{
+//				ChatClient c = new ChatClient();
+//			}
+//		};
+//		clientThread0.start();
+//		Thread clientThread1 = new Thread() // A Client Thread
+//		{
+//			public void run()
+//			{
+//				ChatClient c = new ChatClient();
+//			}
+//		};
+//		clientThread1.start();
+//		Thread clientThread2 = new Thread() // A Client Thread
+//		{
+//			public void run()
+//			{
+//				ChatClient c = new ChatClient();
+//			}
+//		};
+//		clientThread2.start();
+//
+//		Thread clientThread3 = new Thread() // A Client Thread
+//		{
+//			public void run()
+//			{
+//				ChatClient c = new ChatClient();
+//			}
+//		};
+//		clientThread3.start();
+//
+//		Thread clientThread4 = new Thread() // A Client Thread
+//		{
+//			public void run()
+//			{
+//				ChatClient c = new ChatClient(){
+//					
+//				};
+//			}
+//		};
+//		clientThread4.start();
 
 		// // Client Part //////// Client Part //////// Client Part ////////
 		// Client Part ////
