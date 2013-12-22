9bc43a6d27ca10261602f797067175f9f97ddb62
Erik Heinisch <Imkerixus@gmail.com>
2013-12-22 21:08:53 +0100

    No real change, just what comes when coding everywhere, the standard serialVersionUID was exchanged with a specific one, nothing else.

--- SimleNetworkChat/utility/Msg.java
+++ SimleNetworkChat/utility/Msg.java
@@ -6,15 +6,17 @@ import java.util.Date;
 
 public class Msg implements Serializable
 {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -7892210747296752238L;
 	private String content;
+	private String sender;
 	private Object object;
 	private char id;
 	private Date date = new Date();
 
-	public Msg(String p_content, char p_id, Object p_object)
+	public Msg(String p_content, String p_sender, char p_id, Object p_object)
 	{
 		this.content = p_content;
+		this.sender = p_sender;
 		this.id = p_id;
 		this.object = p_object;
 	}
@@ -22,6 +24,11 @@ public class Msg implements Serializable
 	public String getContent()
 	{
 		return content;
+	}
+	
+	public String getSender()
+	{
+		return sender;
 	}
 
 	public char getId()
