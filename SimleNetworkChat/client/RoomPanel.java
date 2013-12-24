a4864a5e11ddaff7a12c0a96ac26b2693668f3ce
Erik Heinisch <Imkerixus@gmail.com>
2013-12-24 23:01:25 +0100

    This is the panel, that will contain all messages in the GUI

--- SimleNetworkChat/client/RoomPanel.java
+++ SimleNetworkChat/client/RoomPanel.java
@@ -12,70 +12,69 @@ public class RoomPanel extends JPanel
 	private static final long serialVersionUID = 170913076776578711L;
 	private ArrayList<Room> roompanels = new ArrayList<Room>();
 	private int roomPanelWidth;
-	
+
 	public RoomPanel(int p_roomPanelwidth)
 	{
 		this.roomPanelWidth = p_roomPanelwidth;
 	}
-	
-	public Room mkRoom(Object[] p_data,final String p_name, final int p_width, final RealClient p_client,boolean p_in)
+
+	public Room mkRoom(Object[] p_data, final String p_name, final int p_width, final RealClient p_client, boolean p_in)
 	{
-		final Room room = new Room(p_client,p_data, p_name, p_width, p_in);
-			room.getBtnMoveaction().addActionListener(new ActionListener() 
+		final Room room = new Room(p_client, p_data, p_name, p_width, p_in);
+		room.getBtnMoveaction().addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
 			{
-				public void actionPerformed(ActionEvent arg0) 
+				if (room.getBtnMoveaction().getText().equals("Enter"))
 				{
-					if(room.getBtnMoveaction().getText().equals("Enter"))
+
+					for (Room rm : roompanels)
 					{
-						
-						for(Room rm : roompanels)
+						if (rm.getClient().getNickname().equals(p_client.getNickname())) // Hat dein Client den selben namen wie meiner
 						{
-							if(rm.getClient().getNickname().equals(p_client.getNickname())) // Hat dein Client den selben namen wie meiner
-							{
-								rm.leaveRoom();
-							}
+							rm.leaveRoom();
 						}
-						room.enterRoom();
 					}
-					else
-					{
-						room.leaveRoom();
-					}
+					room.enterRoom();
+				} else
+				{
+					room.leaveRoom();
 				}
-			});
-			room.setPreferredSize(new Dimension(p_width, room.getList().getPreferredSize().height+room.getMenuBar().getPreferredSize().height));
-			
+			}
+		});
+		room.setPreferredSize(new Dimension(p_width, room.getList().getPreferredSize().height + room.getMenuBar().getPreferredSize().height));
+
 		return room;
 	}
-	
-	public void addRoomPanel(Object[] p_data,String p_name,RealClient p_client,boolean p_in)
+
+	public void addRoomPanel(Object[] p_data, String p_name, RealClient p_client, boolean p_in)
 	{
-			Room room = mkRoom(p_data, p_name, roomPanelWidth, p_client, p_in);
-			roompanels.add(room);
-			this.add(room);
-			mkPreferredSize(); 
+		Room room = mkRoom(p_data, p_name, roomPanelWidth, p_client, p_in);
+		roompanels.add(room);
+		this.add(room);
+		mkPreferredSize();
 	}
-	
+
 	public void rmRoomPanel(int componentIndexToDelete)
 	{
-			this.remove(componentIndexToDelete);
-			mkPreferredSize();
+		this.remove(componentIndexToDelete);
+		mkPreferredSize();
 	}
-	
+
 	public ArrayList<Room> getRooms()
 	{
 		return roompanels;
 	}
-	
+
 	public void mkPreferredSize()
 	{
 		//// Determinates real height of panel to access it RealClient
-			int searchedHeight = 0;
-			for(Room rp : roompanels)
-			{
-				searchedHeight += rp.getPreferredSize().height+5; // +5 is the hgap beetween two RoomPanels
-			}
-			this.setPreferredSize(new Dimension(this.getPreferredSize().width, searchedHeight+5));	// the last +5 are the hgap at the beginning nnot counted above
+		int searchedHeight = 0;
+		for (Room rp : roompanels)
+		{
+			searchedHeight += rp.getPreferredSize().height + 5; // +5 is the hgap beetween two RoomPanels
+		}
+		this.setPreferredSize(new Dimension(this.getPreferredSize().width, searchedHeight + 5));	// the last +5 are the hgap at the beginning nnot counted above
 		//// Determinates real height of panel to access it RealClient
 	}
 
