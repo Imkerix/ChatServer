e8458c05481fc6b41201ced307753aabf0bcad2c
Erik Heinisch <Imkerixus@gmail.com>
2013-12-24 22:58:45 +0100

    Fixed some Protokol bugs and introduced equal code style

--- SimleNetworkChat/server/Server.java
+++ SimleNetworkChat/server/Server.java
@@ -1,6 +1,5 @@
 package server;
 
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
@@ -13,157 +12,158 @@ import utility.Msg;
 public class Server
 {
 	/** Endpoint on the Server */
-	private ServerSocket serversocket = null; 
-	/** A list of all clients that are connected to the server.*/
+	private ServerSocket serversocket = null;
+	/** A list of all clients that are connected to the server. */
 	private List<ServerClient> connectedClients = Collections.synchronizedList(new ArrayList<ServerClient>());
 	private Room[] rooms = new Room[4];
-	
+
 	public Server(int port) throws IOException
 	{
 		rooms[0] = new Room("Spawnroom");
 		rooms[1] = new Room("Redeecke");
 		rooms[2] = new Room("Fanboy Streitecke");
 		rooms[3] = new Room("Pause");
-		
+
 		serversocket = new ServerSocket(port);
 		while (true)
 		{
 			ServerClient thread = new ServerClient(serversocket.accept())
+			{
+				@Override
+				public void run()
 				{
-					@Override
-					public void run()
+					try
 					{
-						try
-						{
-							connectedClients.add(this); // this is a SocketThread
-							rooms[0].addClientToRoom(this); // Spawnroom // this is a SocketThread
-							protokol(this); // this is a SocketThread
-						} catch (IOException e)
-						{
-							e.printStackTrace();
-						}
+						connectedClients.add(this); // this is a SocketThread
+						rooms[0].addClientToRoom(this); // Spawnroom // this is a SocketThread
+						protokol(this); // this is a SocketThread
+					} catch (IOException e)
+					{
+						e.printStackTrace();
 					}
-				};
+				}
+			};
 			thread.start();
 		}
 	}
-	
+
 	public void protokol(ServerClient activeclient) throws IOException
 	{
-		
+
 		boolean shutdown = false;
 		/** Determinates if the Client wants to disconnect */
 		while (!shutdown)
 		{
 			Msg nextCMD = activeclient.getSt().readMsg(activeclient.getClientsocket(), activeclient.getIn());
-			if(nextCMD.getId() == 'b')		//// Broadcast message to everybody on the server
+			if (nextCMD.getId() == 'b')		//// Broadcast message to everybody on the server
 			{
 				broadcast(activeclient, nextCMD);
 			}
-			if(nextCMD.getId() == 's')		//// Message to a specific person on the server
+			if (nextCMD.getId() == 's')		//// Message to a specific person on the server
 			{
 				secredMessage(activeclient, nextCMD);
 			}
-			if(nextCMD.getId() == 'r')		//// Message to all people in a room
+			if (nextCMD.getId() == 'r')		//// Message to all people in a room
 			{
 				roomBroadcast(activeclient, nextCMD);
 			}
-			if(nextCMD.getId() == '+')		//// get me in room / switch room
+			if (nextCMD.getId() == '+')		//// get me in room / switch room
 			{
 				enterRoom(activeclient, nextCMD);
 			}
-			if(nextCMD.getId() == '-')		//// leave room
+			if (nextCMD.getId() == '-')		//// leave room
 			{
 				leaveRoom(activeclient);
 			}
-			if(nextCMD.getId() == 'C')		//// Who is online
+			if (nextCMD.getId() == 'C')		//// Who is online
 			{
 				whoisonline(activeclient);
 			}
-			if(nextCMD.getId() == 'R')		//// Who is online
+			if (nextCMD.getId() == 'R')		//// Who is online
 			{
 				sendExistingRooms(activeclient);
 			}
-			if(nextCMD.getId() == ';')		//// Closes a Connection
+			if (nextCMD.getId() == ';')		//// Closes a Connection
 			{
 				shutdown = true;
 				disconnect(activeclient);
 			}
 		}
 	}
-	
+
 	/** 'b' */
 	private void broadcast(ServerClient activeclient, Msg p_msg)
 	{
-		for (ServerClient sc : connectedClients) 
+		for (ServerClient sc : connectedClients)
 		{
-			if(sc != activeclient)  // dont ask active client for its name he knows that already ^^
-			{
-				sc.getSt().writeMsg(sc.getClientsocket(), sc.getOut(), p_msg);
-			}	
+			sc.getSt().writeMsg(sc.getClientsocket(), sc.getOut(), p_msg);
 		}
-		System.err.println(p_msg.getContent()+" I am the Server");
+		System.err.println(p_msg.getContent() + " I am the Server");
 	}
-	
+
 	/** 's' */
 	private void secredMessage(ServerClient activeclient, Msg nextCMD)
 	{
-		for (ServerClient soth : connectedClients) 
+		for (ServerClient soth : connectedClients)
 		{
-			if(soth.getNickname().equals((String)nextCMD.getObject()))
+			if (soth.getNickname().equals((String) nextCMD.getObject()))
 			{
-				soth.getSt().writeMsg(soth.getClientsocket(), soth.getOut(), new Msg((String)nextCMD.getContent() ,activeclient.getNickname(), 's', null));
+				soth.getSt().writeMsg(soth.getClientsocket(), soth.getOut(), new Msg((String) nextCMD.getContent(), activeclient.getNickname(), 's', null));
+				if (soth != activeclient)// get a copy of the message to the sender again if you dont want to send the message to yourself
+				{
+					activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg((String) nextCMD.getContent(), activeclient.getNickname(), 's', null));
+				}
 			}
 		}
 	}
-	
+
 	/** 'r' */
 	private void roomBroadcast(ServerClient activeclient, Msg p_msg)
 	{
-		for(Room r : rooms)
+		for (Room r : rooms)
 		{
-			if(r.getClientsInRoom().contains(activeclient)) // In which room is the thread who wants to roombroadcast ?
+			if (r.getClientsInRoom().contains(activeclient)) // In which room is the thread who wants to roombroadcast ?
 			{
 				r.roomBroadcast(activeclient, p_msg);
 			}
 		}
 	}
-	
+
 	/** '+' */
 	private void enterRoom(ServerClient activeclient, Msg nextCMD)
-	{	
-		for(Room r : rooms)
+	{
+		for (Room r : rooms)
 		{
-			if(r.getClientsInRoom().contains(activeclient)) // which room shell be left ?
+			if (r.getClientsInRoom().contains(activeclient)) // which room shell be left ?
 			{
 				r.rmClientFromRoom(activeclient); // leave
 			}
-			if(r.getName().equals(nextCMD.getContent())) // which room shell be entered ?
+			if (r.getName().equals(nextCMD.getContent())) // which room shell be entered ?
 			{
 				r.addClientToRoom(activeclient); // enter
 			}
 		}
 	}
-	
+
 	/** '-' */
 	private void leaveRoom(ServerClient activeclient)
 	{
-		for(Room r : rooms)
+		for (Room r : rooms)
 		{
-			if(r.getClientsInRoom().contains(activeclient)) 
+			if (r.getClientsInRoom().contains(activeclient))
 			{
-				r.rmClientFromRoom(activeclient); 
+				r.rmClientFromRoom(activeclient);
 			}
 		}
 	}
-	
+
 	/** 'C' */
 	private void whoisonline(ServerClient activeclient) throws IOException
 	{
-		ArrayList<String> clientsOnline = new ArrayList<String>(); 
+		ArrayList<String> clientsOnline = new ArrayList<String>();
 		for (ServerClient sc : connectedClients)
 		{
-			if(sc != activeclient)  // dont ask active client for its name he knows that already ^^
+			if (sc != activeclient)  // dont ask active client for its name he knows that already ^^
 			{
 				clientsOnline.add(sc.getNickname());
 			}
@@ -175,21 +175,21 @@ public class Server
 		{
 			e.printStackTrace();
 		}
-		activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg("","server",'C',clientsOnline));
+		activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg("", "server", 'C', clientsOnline));
 	}
-	
+
 	/** 'R' */
 	private void sendExistingRooms(ServerClient activeclient)
 	{
-		HashMap<String,ArrayList<String>> existingRooms = new HashMap<String,ArrayList<String>>();
+		HashMap<String, ArrayList<String>> existingRooms = new HashMap<String, ArrayList<String>>();
 		for (Room r : rooms)
 		{
 			ArrayList<String> users = new ArrayList<String>();
-			for(ServerClient sc : r.getClientsInRoom())
+			for (ServerClient sc : r.getClientsInRoom())
 			{
 				users.add(sc.getNickname());
 			}
-			existingRooms.put(r.getName(),users);
+			existingRooms.put(r.getName(), users);
 		}
 		try
 		{
@@ -198,18 +198,18 @@ public class Server
 		{
 			e.printStackTrace();
 		}
-		activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg("","server",'R',existingRooms));
+		activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg("", "server", 'R', existingRooms));
 	}
-	
+
 	/** ';' */
 	public void disconnect(ServerClient activeclient)
 	{
-		try {
+		try
+		{
 			leaveRoom(activeclient); // The Client needs to be away completely !
 			connectedClients.remove(activeclient);
 			activeclient.getSt().writeMsg(activeclient.getClientsocket(), activeclient.getOut(), new Msg("", "server", ';', null));
 			activeclient.getClientsocket().close();
-			System.out.println("is down");
 		} catch (IOException e)
 		{
 			e.printStackTrace();
