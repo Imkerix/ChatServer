3351ffacbf0c20907bf14146c535c7d195445d30
Erik Heinisch <Imkerixus@gmail.com>
2014-01-04 16:12:19 +0100

    -mkRoom stuff I gladly announce, that I do not longer use this bad implementaton, so it could be removed.
    I also uncommented rmRoom, because there is no such functionality at
    this point.

--- SimleNetworkChat/client/RoomPanel.java
+++ SimleNetworkChat/client/RoomPanel.java
@@ -18,48 +18,20 @@ public class RoomPanel extends JPanel
 		this.roomPanelWidth = p_roomPanelwidth;
 	}
 
-	public Room mkRoom(Object[] p_data, final String p_name, final int p_width, final RealClient p_client, boolean p_in)
+	public void addRoom(Object[] p_data, String p_name, RealClient p_client, boolean p_in)
 	{
-		final Room room = new Room(p_client, p_data, p_name, p_width, p_in);
-		room.getBtnMoveaction().addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				if (room.getBtnMoveaction().getText().equals("Enter"))
-				{
-
-					for (Room rm : roompanels)
-					{
-						if (rm.getClient().getNickname().equals(p_client.getNickname())) // Hat dein Client den selben namen wie meiner
-						{
-							rm.leaveRoom();
-						}
-					}
-					room.enterRoom();
-				} else
-				{
-					room.leaveRoom();
-				}
-			}
-		});
-		room.setPreferredSize(new Dimension(p_width, room.getList().getPreferredSize().height + room.getMenuBar().getPreferredSize().height));
-
-		return room;
-	}
-
-	public void addRoomPanel(Object[] p_data, String p_name, RealClient p_client, boolean p_in)
-	{
-		Room room = mkRoom(p_data, p_name, roomPanelWidth, p_client, p_in);
+		Room room = new Room(p_client, p_data, p_name, roomPanelWidth, p_in);
+		room.setPreferredSize(new Dimension(roomPanelWidth, room.getList().getPreferredSize().height + room.getMenuBar().getPreferredSize().height));
 		roompanels.add(room);
 		this.add(room);
 		mkPreferredSize();
 	}
 
-	public void rmRoomPanel(int componentIndexToDelete)
-	{
-		this.remove(componentIndexToDelete);
-		mkPreferredSize();
-	}
+//	public void rmRoom(int componentIndexToDelete) // could be usefull some day
+//	{
+//		this.remove(componentIndexToDelete);
+//		mkPreferredSize();
+//	}
 
 	public ArrayList<Room> getRooms()
 	{
@@ -76,6 +48,18 @@ public class RoomPanel extends JPanel
 		}
 		this.setPreferredSize(new Dimension(this.getPreferredSize().width, searchedHeight + 5));	// the last +5 are the hgap at the beginning nnot counted above
 		//// Determinates real height of panel to access it RealClient
+	}
+	
+	public Room getActiveRoom(String nickname)
+	{
+		for(Room r : roompanels)
+		{
+			if(r.getClient().getNickname().equals(nickname))
+			{
+				return r;
+			}
+		}
+		return null;
 	}
 
 }
